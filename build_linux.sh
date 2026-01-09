dfe976188e0de0767ef5ed26ef56dafd88ee0631
SiputBiru <hillsforrest03@gmail.com>
2026-01-09 12:33:08 +0700

    fixing the assets copy things

--- build_linux.sh
+++ build_linux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 OUT_DIR="bin/linux"
-EXE_NAME="Raylib-PSX-Odin.exe"
+EXE_NAME="Raylib-PSX-Odin"
 
 
 mkdir -p $OUT_DIR
@@ -11,9 +11,9 @@ if [ ! -d "$OUT_DIR/shaders" ]; then
     cp shaders/* $OUT_DIR/shaders/
 fi
 
-if [ ! -d "$OUT_DIR/shaders" ]; then 
-    mkdir -p $OUT_DIR/shaders
-    cp shaders/* $OUT_DIR/shaders/
+if [ ! -d "$OUT_DIR/assets" ]; then 
+    mkdir -p $OUT_DIR/assets
+    cp assets/* $OUT_DIR/assets/
 fi
 
 if [ -f "$OUT_DIR/$EXE_NAME" ]; then
